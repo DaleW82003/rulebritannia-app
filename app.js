3b0c661cfbb1c6b80280a121834319f4deb01fac
DaleW82003 <dalew82003@hotmail.com>
2026-02-11 09:39:43 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// placeholder
+fetch("data/demo.json")
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("news-cards").innerHTML = "News system connected ✔";
+    document.getElementById("order-paper").innerHTML = "Legislation system connected ✔";
+    document.getElementById("economy").innerHTML = "Economy system connected ✔";
+  });
